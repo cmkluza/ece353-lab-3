2b6393d33422883494078eb0cdfbd7da6ffa8da3
Laura DeBurgo <43252101+ldeburgo@users.noreply.github.com>
2018-11-21 23:05:26 -0500

    Update mips_sim.c

--- mips_sim.c
+++ mips_sim.c
@@ -406,25 +406,7 @@ void IF(void) {
 }
 
 // TODO
-void ID(void) {
-    IF_Cycle_Counter++;
-	struct inst curr_inst;
-	curr_inst= IM[PC];                                            // create local copy of the instruction to be executed
-
-	if (IF_ID_Flag==0){                                           // check if latch is empty
-		if (curr_inst.op==HALT){                  
-	    	IF_ID_latch= curr_inst;                               // send the halt instruction to the next stage
-			IF_ID_Flag=1;
-		}
-		if (IF_Cycle_Counter>=c){                
-			IF_ID_latch= curr_inst;                               // send the instruction to the next stage
-			PC= PC + 4;                                           // change PC to the next instruction
-			IF_ID_Flag=1;                                         // set flag IF/ID latch not empty
-			IF_Cycle_Counter=0;
-			IF_WorkCycles=IF_WorkCycles+c;                        // updates count of useful cycles
-		}
-	}
-}
+void ID(void) {}
 
 // TODO
 void EX(void) {}
