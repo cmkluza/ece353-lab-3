a0deccad39371a9133cfc7cdfbfed7a1d92f01d7
Laura DeBurgo <43252101+ldeburgo@users.noreply.github.com>
2018-11-21 23:41:11 -0500

    Add files via upload

--- ECE353-Lab3.c
+++ ECE353-Lab3.c
@@ -2,74 +2,76 @@
 
 void* IF() {
 
-  struct inst curr_inst;
-	
-	if (IF_ID_Flag==0){                    // check if latch is empty
-		curr_inst= IM[PC];             // create local copy of the instruction to be executed
-		IF_ID_latch= curr_inst;        // send the instruction to the next stage
-		PC= PC + 4;                    // change PC to the next instruction
-		IF_ID_Flag=1;                  // set flag IF/ID latch not empty
+    IF_Inst_cycles++;
+	struct inst curr_inst;
+	curr_inst= IM[PC];                                            // create local copy of the instruction to be executed
+
+	if (IF_ID_Flag==0){                                           // check if latch is empty
+		if (curr_inst.op==HALT){                  
+	    	IF_ID_latch= curr_inst;                               // send the halt instruction to the next stage
+			IF_ID_Flag=1;
+		}
+		if (IF_Inst_cycles>=c){                
+			IF_ID_latch= curr_inst;                               // send the instruction to the next stage
+			PC= PC + 4;                                           // change PC to the next instruction
+			IF_ID_Flag=1;                                         // set flag IF/ID latch not empty
+			IF_Inst_cycles=0;
+			IF_WorkCycles=IF_WorkCycles+c;                        // updates count of useful cycles
+		}
 	}
+	
 }
+
 
  void* EX() {
  
- 	struct curr_inst;
- 
- 	if(ID_EX_Flag==1 && EX_Cycle_Counter==0){
+ 	if(ID_EX_Flag==1 && EX_Inst_Cycles==0){
  		struct curr_inst= ID_EX_latch;
  		ID_EX_Flag=0;
  	}
  
- 	EX_Cycle_Counter=EX_Cycle_Counter+1;
- 
-        // ADD operation
+ 	EX_Inst_Cycles++;
+
+    // ADD operation
 	if (curr_inst.op==ADD){
-		if (EX_Cycle_Counter>= ){
+		if (EX_Inst_Cycles>=n){
 			curr_inst.EX_result= (int16_t) curr_inst.rs + curr_inst.rt;
-			EX_Cycle_Counter=0;
 		}
 	}
 	
 	// ADDI operation
 	if (curr_inst.op==ADDI){
-		if (EX_Cycle_Counter>= ){
-			curr_inst.EX_result= (int16_t) curr_inst.rt + curr_inst.immediate;
-			EX_Cycle_Counter=0;
+		if (EX_Inst_Cycles>=n){
+			curr_inst.EX_result= (int16_t) curr_inst.rs + curr_inst.immediate;
 		}
 	}
 	
 	//BEQ operation
 	if (curr_inst.op==BEQ){
-	
-		if (EX_Cycle_Counter>= ){
+		if (EX_Inst_Cycles>=n){
 			curr_inst.EX_result= (int16_t) curr_inst.rt - curr_inst.rs;
 			if (curr_inst.EX_result==0) PC = PC + 4 + 4 * (curr_inst.immediate);
-			EX_Cycle_Counter=0;
 		}
 	}
 	
 	//LW and SW operation, not sure how to do this
 	if (curr_inst.op==LW || curr_inst..op==SW){
-		if (EX_Cycle_Counter>= ){
-			//curr_inst.EX_result= (int16_t) 
-			EX_Cycle_Counter=0;
-		}
-	}
-	
-	//MUL operation
-	if (curr_inst.op==MUL){
-		if (EX_Cycle_Counter>= ){
-			curr_inst.EX_result= (int16_t) curr_inst.rs * curr_inst.rt;
-			EX_Cycle_Counter=0;
+		if (EX_Inst_Cycles>=n){
+			curr_inst.EX_result= (int16_t) curr_inst.rs + curr_inst.immediate; 
 		}
 	}
 	
 	//SUB operation
 	if (curr_inst.op==SUB){
-		if (EX_Cycle_Counter>= ){
+		if (EX_Inst_Cycles>=n){
 			curr_inst.EX_result= (int16_t) curr_inst.rs - curr_inst.rt;
-			EX_Cycle_Counter=0;
+		}
+	}
+	
+	//MUL operation
+	if (curr_inst.op==MUL){
+		if (EX_Inst_Cycles>=m){
+			curr_inst.EX_result= (int16_t) curr_inst.rs * curr_inst.rt;
 		}
 	}
 	
@@ -77,6 +79,9 @@ void* IF() {
 	if (EX_MEM_Flag==0){
 		EX_MEM_latch=curr.inst;
 		EX_MEM_Flag=1;
+		EX_Inst_Cycles=0;
+		if (curr_inst.op==MUL) EX_WorkCycles=EX_WorkCycles+m;
+		else if (curr_inst.op!=HALT && curr_inst.op!=MUL) EX_WorkCycles=EX_WorkCycles+n;
 	}
 		
 }
