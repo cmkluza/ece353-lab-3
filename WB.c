a5a15c73d16cccb61d2d2dd667559f5d317e02e8
Dametreuss <dametreuss@live.com>
2018-11-23 15:45:52 -0500

    Update and rename WB_Prog.c to WB.c

---
+++ WB.c
@@ -0,0 +1,41 @@
+static int wbCount;  //WB useful counter
+void WB()
+{
+	//Note: SW is done in MEM I think so it's not included
+	if(MEM_WB_Flag == 1) //If there is new info...
+	{
+		if(MEM_WB_Latch.op == HALT) //If HALT, 
+			{
+				WB_Halt_Flag == 1; //set halt flag and do nothing else
+			}
+		else if(MEM_WB_Latch.op == ADD) //if ADD use [RD] 
+			{
+				Registers[MEM_WB_Latch.rd] = MEM_WB_Latch.result;
+				wbCount+=1; //increase useful counter
+			}
+		else if(MEM_WB_Latch.op == ADDI) //if ADDI use [RT]
+			{
+				Registers[MEM_WB_Latch.rt] = MEM_WB_Latch.result;
+				wbCount+=1;	
+			} 
+		else if(MEM_WB_Latch.op == SUB) //if SUB use [RD]
+			{
+				Registers[MEM_WB_Latch.rd] = MEM_WB_Latch.result;
+				wbCount+=1;
+			} 
+		else if(MEM_WB_Latch.op == MUL) //if MULT use [RD]
+			{
+				Registers[MEM_WB_Latch.rd] = MEM_WB_Latch.result;
+				wbCount+=1;
+			} 
+		else if(MEM_WB_Latch.op == LW) //if LW use [RT]
+			{
+				Registers[MEM_WB_Latch.rt] = MEM_WB_Latch.result;
+				wbCount+=1;
+			} 
+										//SW is done in MEM
+	}
+
+	MEM_WB_Flag == 0; //WB work is done
+
+}
