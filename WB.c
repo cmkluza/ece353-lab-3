c4cd7531850423d8169b511212a668192ff4d539
KyleMcWherter <43252090+KyleMcWherter@users.noreply.github.com>
2018-11-23 22:12:17 -0500

    Update WB.c

--- WB.c
+++ WB.c
@@ -3,31 +3,31 @@ void WB()
 	//Note: SW is done in MEM I think so it's not included
 	if(MEM_WB_Flag == 1) //If there is new info...
 	{
-		if(MEM_WB_Latch.inst_op == HALT) //If HALT, 
+		if(MEM_WB_latch.inst_op == HALT) //If HALT, 
 			{
 				WB_Halt_Flag == 1; //set halt flag and do nothing else
 			}
-		else if(MEM_WB_Latch.inst_op == ADD) //if ADD use [RD] 
+		else if(MEM_WB_latch.inst_op == ADD) //if ADD use [RD] 
 			{
 				Registers[MEM_WB_Latch.rd] = MEM_WB_Latch.result;
 				WB_WorkCycles++; //increase useful counter
 			}
-		else if(MEM_WB_Latch.inst_op == ADDI) //if ADDI use [RT]
+		else if(MEM_WB_latch.inst_op == ADDI) //if ADDI use [RT]
 			{
 				Registers[MEM_WB_Latch.rt] = MEM_WB_Latch.result;
 				WB_WorkCycles++;	
 			} 
-		else if(MEM_WB_Latch.inst_op == SUB) //if SUB use [RD]
+		else if(MEM_WB_latch.inst_op == SUB) //if SUB use [RD]
 			{
 				Registers[MEM_WB_Latch.rd] = MEM_WB_Latch.result;
 				WB_WorkCycles++;
 			} 
-		else if(MEM_WB_Latch.inst_op == MUL) //if MULT use [RD]
+		else if(MEM_WB_latch.inst_op == MUL) //if MULT use [RD]
 			{
 				Registers[MEM_WB_Latch.rd] = MEM_WB_Latch.result;
 				WB_WorkCycles++;
 			} 
-		else if(MEM_WB_Latch.inst_op == LW) //if LW use [RT]
+		else if(MEM_WB_latch.inst_op == LW) //if LW use [RT]
 			{
 				Registers[MEM_WB_Latch.rt] = MEM_WB_Latch.result;
 				WB_WorkCycles++;
